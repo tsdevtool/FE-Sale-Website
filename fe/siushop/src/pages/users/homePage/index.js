c3775ea3887352923f3510ea0425ac8e90fe0b66
ThanhSieu2k3 <122494878+tsdevtool@users.noreply.github.com>
2024-05-16 20:14:11 +0700

    Done b3

--- fe/siushop/src/pages/users/homePage/index.js
+++ fe/siushop/src/pages/users/homePage/index.js
@@ -2,9 +2,8 @@ import { memo } from "react";
 
 const HomePage = () =>{
     return <>
-        <h1>Header</h1>
-        <h1>Body</h1>
-        <h1>Footer</h1>
+        <h1>HomePage</h1>
+
     </>;
 };
 
---
+++ fe/siushop/src/pages/users/theme/header/index.js
@@ -0,0 +1,9 @@
+import { memo } from "react";
+
+const Header = () =>{
+    return <>
+        <h1>Header</h1>
+    </>;
+};
+
+export default memo(Header);
